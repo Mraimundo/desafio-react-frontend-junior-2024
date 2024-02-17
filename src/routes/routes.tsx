587d22402a716e1983fc645027b5f121c7f1a763
Mraimundo <mdumundo18@gmail.com>
2024-02-16 21:44:27 -0300

    chore: route page refactoring

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -7,17 +7,32 @@ import { FilterType, TaskType, TodoList } from "../pages/TodoList/todo-list";
 export function AppRoutes() {
   const [tasks, setTasks] = useState<TaskType[]>([]);
   const [activeFilter, setActiveFilter] = useState<FilterType>("all");
-
   const navigate = useNavigate();
 
   useEffect(() => {
     setTasks(data.todos);
   }, []);
 
-  const handleFilterChange = (filter: FilterType) => {
+  const handleFilterChange = (filter: FilterType): void => {
     setActiveFilter(filter);
     navigate(filter === "all" ? "/" : `/${filter}`);
   };
+
+  const filterMapping: Record<string, FilterType> = {
+    "/": "all",
+    "/all": "all",
+    "/active": "active",
+    "/completed": "completed",
+  };
+
+  const renderTodoList = (filter: FilterType): JSX.Element => (
+    <TodoList
+      filter={filter}
+      tasks={tasks}
+      setTasks={setTasks}
+      onFilterChange={handleFilterChange}
+    />
+  );
 
   if (window.location.pathname === "/") {
     return <Navigate to="/all" />;
@@ -25,50 +40,9 @@ export function AppRoutes() {
 
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <TodoList
-            filter="all"
-            tasks={tasks}
-            setTasks={setTasks}
-            onFilterChange={handleFilterChange}
-          />
-        }
-      />
-      <Route
-        path="/all"
-        element={
-          <TodoList
-            filter={activeFilter}
-            tasks={tasks}
-            setTasks={setTasks}
-            onFilterChange={handleFilterChange}
-          />
-        }
-      />
-      <Route
-        path="/active"
-        element={
-          <TodoList
-            filter="active"
-            tasks={tasks}
-            setTasks={setTasks}
-            onFilterChange={handleFilterChange}
-          />
-        }
-      />
-      <Route
-        path="/completed"
-        element={
-          <TodoList
-            filter="completed"
-            tasks={tasks}
-            setTasks={setTasks}
-            onFilterChange={handleFilterChange}
-          />
-        }
-      />
+      {Object.entries(filterMapping).map(([path, filter]) => (
+        <Route key={path} path={path} element={renderTodoList(filter)} />
+      ))}
     </Routes>
   );
 }
